a405e73f769756691133b6f2fd6ba90ee8803632
NAD <maryunani52@gmail.com>
2025-06-23 21:54:04 +0700

    commit 5

---
+++ src/TicTacToe5Login/State.java
@@ -0,0 +1,9 @@
+package TicTacToe5Login;
+
+
+/**
+ *  The enum State contains the various game states of the TTT game
+ */
+public enum State {  // to save as "State.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
