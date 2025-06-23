e99cf5c7f592a4474645fd381e170cfff514d7a8
Sastralodon <basanta.sastra@gmail.com>
2025-06-23 21:48:05 +0700

    Repair Error

---
+++ src/TicTacToe7/State.java
@@ -0,0 +1,8 @@
+package TicTacToe7;
+
+/**
+ *  The enum State contains the various game states of the TTT game
+ */
+public enum State {  // to save as "State.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
