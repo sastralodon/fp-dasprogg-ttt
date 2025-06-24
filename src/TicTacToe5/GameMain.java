afdd3d9f19b3f219c646cd840091470f24d58c15
NAD <maryunani52@gmail.com>
2025-06-24 11:04:40 +0700

    commit databes

--- src/TicTacToe5/GameMain.java
+++ src/TicTacToe5/GameMain.java
@@ -1,10 +1,13 @@
+package TicTacToe5;
+
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 /**
  * Tic-Tac-Toe: Two-player Graphic version with better OO design.
  * The Board and Cell classes are separated in their own classes.
- *///
+ */
 public class GameMain extends JPanel {
     private static final long serialVersionUID = 1L; // to prevent serializable warning
 
@@ -40,14 +43,16 @@ public class GameMain extends JPanel {
                             && board.cells[row][col].content == Seed.NO_SEED) {
                         // Update cells[][] and return the new game state after the move
                         currentState = board.stepGame(currentPlayer, row, col);
-                        if (currentState == State.PLAYING) {
-                            SoundEffect.EAT_FOOD.play();
-                        } else {
-                            SoundEffect.DIE.play();
-                        }
                         // Switch player
                         currentPlayer = (currentPlayer == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
                     }
+                    if (currentState == State.PLAYING) {
+                        SoundEffect.DIE.play();
+                    } else if(currentState == State.DRAW){
+                        SoundEffect.EAT_FOOD.play();
+                    } else{
+                        SoundEffect.EXPLODE.play();
+     }
                 } else {        // game over
                     newGame();  // restart the game
                 }
