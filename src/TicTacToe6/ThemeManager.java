b1e280761312f164ec273d09f8abea5b6ae41196
Tic Tac Toe <namakuaidasafa@gmail.com>
2025-06-25 16:13:27 +0700

    TicTacToe6 ;

--- src/TicTacToe6/ThemeManager.java
+++ src/TicTacToe6/ThemeManager.java
@@ -1,4 +1,4 @@
-package TicTacToe5;
+package TicTacToe6;
 
 import javax.imageio.ImageIO;
 import java.awt.Image;
@@ -29,6 +29,7 @@ public class ThemeManager {
             return null;
         }
     }
+
     public static Clip loadBGM(String filename) {
         String path = "/themes/" + selectedTheme + "/" + filename;
         try (InputStream is = ThemeManager.class.getResourceAsStream(path)) {
@@ -45,7 +46,4 @@ public class ThemeManager {
             return null;
         }
     }
-
-
-
 }
